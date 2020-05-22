17d2e1eebde26abb129b7c9ca543f23643cb83e8
tignioj <859897994@qq.com>
2020-05-22 22:05:17 +0800

    冻结App功能完成

--- app/src/main/java/com/example/freezeappdemo1/MainActivity.java
+++ app/src/main/java/com/example/freezeappdemo1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.freezeappdemo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -19,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE_WRITE_SETTINGS = 0x10;
     EditText editText;
 
     @Override
@@ -28,81 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.editText2);
-        editText.setText("pm disable-user --user 0 com.example.myapp");
 
-        editText.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                // If the event is a key-down event on the "enter" button
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    freeze(v);
-                    return true;
-                }
-                return false;
-            }
-        });
+        //执行的命令
+        editText.setText("com.example.myapp");
     }
 
-    boolean canWrite;
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_CODE_WRITE_SETTINGS) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                //Settings.System.canWrite方法检测授权结果
-                if (Settings.System.canWrite(getApplicationContext())) {
-                    Log.d("myTag", "你获取了权限");
-                    canWrite = true;
-                } else {
-                    Log.d("myTag", "你拒绝了权限");
-                    canWrite = false;
-                }
-            }
-        }
-    }
 
     /**
-     * 点击按钮执行
+     * 冻结App
      * @param view
      */
     public void freeze(View view) {
-        /**
-         * 检测系统修改权限
-         */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.System.canWrite(MainActivity.this)) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS,
-                        Uri.parse("package:" + getPackageName()));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS);
-            } else {
-                // 如果有权限做些什么
-                canWrite = true;
-            }
-        }
-        Process process = null;
-        try {
-            String command = editText.getText().toString();
-            if (canWrite) {
-                process = Runtime.getRuntime().exec(command);
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                String line;
-                StringBuilder sb = new StringBuilder();
-                while ((line = bufferedReader.readLine()) != null) {
-                    Log.d("myTag", line);
-                    sb.append(line + "\n");
-                }
-                Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
-                editText.setText("");
-            } else {
-                Toast.makeText(getApplicationContext(), "没有权限", Toast.LENGTH_SHORT).show();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String packageName = editText.getText().toString();
+        DeviceMethod.getInstance(getApplicationContext()).freeze(packageName, true);
     }
 
-
+    /**
+     * 解冻App
+     * @param view
+     */
+    public void  unfreeze(View view) {
+        String packageName = editText.getText().toString();
+        DeviceMethod.getInstance(getApplicationContext()).freeze(packageName, false);
+    }
 }
---
+++ app/src/main/java/com/example/freezeappdemo1/MyDeviceAdminReceiver.java
@@ -0,0 +1,62 @@
+package com.example.freezeappdemo1;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyDeviceAdminReceiver  extends DeviceAdminReceiver {
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        // 设备管理：可用
+        Toast.makeText(context, "设备管理：可用", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(final Context context, Intent intent) {
+        // 设备管理：不可用
+        Toast.makeText(context, "设备管理：不可用", Toast.LENGTH_SHORT).show();
+        //如果取消了激活就再次提示激活
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                DeviceMethod.getInstance(context.getApplicationContext()).onActivate();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public CharSequence onDisableRequested(Context context, Intent intent) {
+           /* // 这里处理 不可编辑设备。这里可以造成死机状态
+            Intent intent2 = new Intent(context, NoticeSetting.class);
+            intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent2);
+            context.stopService(intent);// 是否可以停止*/
+
+        return "这是一个可选的消息，警告有关禁止用户的请求";
+    }
+
+    @Override
+    public void onPasswordChanged(Context context, Intent intent) {
+        // 设备管理：密码己经改变
+        Toast.makeText(context, "设备管理：密码己经改变", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPasswordFailed(Context context, Intent intent) {
+        // 设备管理：改变密码失败
+        Toast.makeText(context, "设备管理：改变密码失败", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPasswordSucceeded(Context context, Intent intent) {
+        // 设备管理：改变密码成功
+        Toast.makeText(context, "设备管理：改变密码成功", Toast.LENGTH_SHORT).show();
+    }
+}
