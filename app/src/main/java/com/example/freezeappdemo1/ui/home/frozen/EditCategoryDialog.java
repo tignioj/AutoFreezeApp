0881a4f7fb2cb4a42584ecab16a46ac5a7bcdb2e
tignioj <859897994@qq.com>
2020-05-27 08:18:18 +0800

    Dialog的UI界面修复

--- app/src/main/java/com/example/freezeappdemo1/ui/home/frozen/EditCategoryDialog.java
+++ app/src/main/java/com/example/freezeappdemo1/ui/home/frozen/EditCategoryDialog.java
@@ -85,7 +85,6 @@ public class EditCategoryDialog extends DialogFragment {
                                         }
                                     });
                                 }
-
                             }
                         }).start();
                     }
